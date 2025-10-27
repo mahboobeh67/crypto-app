3aae6e6eeb11d7790d8a7a63cbbcf2b70c7100b6
mahboobehnaderian <mahboobehnaderian1367@gmail.com>
2025-10-27 14:58:30 +0330

    added tabel

--- src/components/module/TableCoins.jsx
+++ src/components/module/TableCoins.jsx
@@ -1,9 +1,75 @@
-
-function TableCoins({coins}) {
-  console.log(coins)
+import chart_up from "../../assets/chart-up.svg";
+import chart_down from "../../assets/chart-down.svg";
+import { DNA } from "react-loader-spinner";
+import styles from "./TableCoins.module.css"
+function TableCoins({ coins, isLoading }) {
   return (
-    <div>TableCoins</div>
-  )
+    <div className={styles.container}>
+      {isLoading ? (
+        <div>
+          <DNA height="100" width="100" />
+        </div>
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins?.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
-export default TableCoins
+export default TableCoins;
+
+const TableRow = ({
+  coin: {
+    image,
+    name,
+    symbol,
+    total_volume,
+    price_change_percentage_24h: price_change,
+    current_price,
+  },
+}) => {
+  
+  const safeChange = price_change ?? 0;
+  const isPositive = safeChange > 0;
+
+  return (
+    <tr>
+      <td>
+        <div className={styles.symbol}>
+          <img src={image} alt={name}  />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {safeChange.toFixed(2)}%
+      </td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img
+          src={isPositive ? chart_up : chart_down}
+          alt="trend"
+          
+        />
+      </td>
+    </tr>
+  );
+};
+
--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import TableCoins from "../module/TableCoins.jsx"
+import { getCoinList } from "../../services/cryptoApi.js";
 
 function HomePage() {
   const [coins, setCoins]= useState([])
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&local=en&x_cg_demo_api_key=CG-2hEdyVeWV511V4NMo1vj5t7v"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));;
+    const getData = async () => {
+      const res = await fetch (getCoinList())
+      const json = await res.json()
+      setCoins(json)
+      setIsLoading(false)
+    }
+    getData()
   }, []);
-  return <div><TableCoins coins={coins}/></div>;
+  return <div><TableCoins coins={coins} isLoading= {isLoading}/></div>;
 }
 
 export default HomePage;
