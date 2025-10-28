2a1590bb6eba679325d9a55fd35d45450583ecbc
mahboobehnaderian <mahboobehnaderian1367@gmail.com>
2025-10-28 13:45:34 +0330

    added search and currency

---
+++ src/components/module/Search.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import { searchCoin } from "../../services/cryptoApi";
+
+
+function Search({currency, setCurrency}) {
+    const [text , setText] = useState("");
+    const [coin, setCoin] = useState([])
+
+    useEffect(() => {
+        const controller = new AbortController();
+        if(!text) return;
+     const search = async () => {
+
+        try{
+             const res = await fetch(searchCoin(text), {signal: controller.signal})
+      const json = await res.json()
+      if (json.coin) {setCoin(json.coin)}else{alert(json.status.error_message)}
+        }catch(error){
+    if(error.name !== "AbortError"){
+        alert(error.message)
+    }
+    }
+     
+     }
+    
+      search()
+      return () => controller.abort();
+    }, [text])
+    
+  return (
+    <div>
+    
+    <input type="text" placeholder="Search" value={text} onChange={(e) => setText(e.target.value)} />
+    <select value={currency} onChange={(e) => setCurrency(e.target.value)} >
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+    </select>
+    <div>
+        <ul>
+            {coin.map((coin) => (<li key={coin.id}>{}</li>))}
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Search
